3e0ae8fe8655ef0e94c98d55b303479f7916a40c
YoannYWD <yoann.guidec@gmail.com>
2021-09-10 14:38:41 +0200

    correction cardinalité

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -19,8 +19,8 @@ class Comment extends Model
     public function user() {
         return $this->belongsTo(User::class);
     }
-    public function comment() {
-        return $this->belongsTo(Comment::class);
+    public function destination() {
+        return $this->belongsTo(Destination::class);
     }
 
 }
